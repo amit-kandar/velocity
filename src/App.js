eddb5a658c2ce39bc4997ce452c7fd0dea76281d
Amit Kandar <kandaramit2001@gmail.com>
2024-03-01 15:57:10 +0530

    Added few components

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import SubNavbar from "./components/SubNavbar";
+import Notification from "./components/Notification";
 
 function App() {
   return (
     <Router>
+      <Notification />
+      <Navbar />
+      <SubNavbar />
       <Routes>
         <Route />
       </Routes>
