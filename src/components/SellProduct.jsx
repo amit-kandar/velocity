fd9de0b51f2e009d38bc3858ede89c162b0a6064
Amit Kandar <kandaramit2001@gmail.com>
2024-03-04 13:37:34 +0530

    Added SellProduct

---
+++ src/components/SellProduct.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function SellProduct() {
+    return (
+        <div className='mt-5 p-5 bg-white rounded-2xl'>
+            <h1 className='text-left text-3xl font-bold'>Lorem ipsum dolor.</h1>
+            <div className='mt-5 grid place-items-center gap-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
+                <div className='w-56 h-56 bg-gray-500 rounded-md'></div>
+                <div className='w-56 h-56 bg-gray-500 rounded-md'></div>
+                <div className='w-56 h-56 bg-gray-500 rounded-md'></div>
+                <div className='w-56 h-56 bg-gray-500 rounded-md'></div>
+                <div className='w-56 h-56 bg-gray-500 rounded-md'></div>
+                <div className='w-56 h-56 bg-gray-500 rounded-md'></div>
+            </div>
+        </div>
+    )
+}
+
+export default SellProduct
