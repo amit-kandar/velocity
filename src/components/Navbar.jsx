690c3e93b6bab789f1ca2e5021f9dffaf3f76620
Amit Kandar <kandaramit2001@gmail.com>
2024-03-04 16:11:10 +0530

    Added country dropdown to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
 import React, { useState } from 'react';
 import { LOGO } from '../constants'
 
+
+const countryCodes = [
+    'US',
+    'CA',
+    'GB',
+    'AU',
+    'DE',
+    'FR',
+    'JP',
+    'IN',
+    'BR',
+    'MX',
+];
+
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [selectedCountry, setSelectedCountry] = useState(null)
+
+    const handleDropdown = () => {
+        setIsDropdownOpen(prev => !prev);
+    }
+
+    const handleSelectCountry = (country) => {
+        setSelectedCountry(country)
+    }
 
     const profileImage = false
     const handleOpen = () => {
@@ -14,6 +38,30 @@ function Navbar() {
                 <img src={LOGO} alt="logo" className='w-full h-full rounded-full' />
             </div>
             <div className='hidden lg:flex justify-evenly items-center gap-8'>
+                <div className='relative text-black text-2xl cursor-pointer border border-gray-500 rounded-full px-2 flex justify-center items-center' onClick={handleDropdown}>
+                    {
+                        selectedCountry === null
+                            ? <i className='bx bx-globe text-base' ></i>
+                            : <span className='text-base font-medium'>{selectedCountry}</span>
+                    }
+                    {
+                        isDropdownOpen
+                            ? <i className='bx bx-chevron-down text-2xl' ></i>
+                            : <i className='bx bx-chevron-up text-2xl' ></i>
+                    }
+                    {
+                        isDropdownOpen &&
+                        <div className={`${isDropdownOpen ? "block" : "hidden"} absolute w-auto h-auto  top-14 left-0 bg-white z-50 p-3`}>
+                            {
+                                countryCodes.map((item, index) => {
+                                    return (
+                                        <div key={index} className='pr-5 text-sm font-medium font-roboto hover:bg-gray-200' onClick={() => handleSelectCountry(item)}>{item}</div>
+                                    )
+                                })
+                            }
+                        </div>
+                    }
+                </div>
                 <div className='relative text-black text-2xl cursor-pointer'>
                     <span className='absolute flex justify-center items-center top-0 right-0 text-xs text-white font-bold w-4 h-4 bg-red-500 rounded-full '>2</span>
                     <i className='bx bxs-bell'></i>
