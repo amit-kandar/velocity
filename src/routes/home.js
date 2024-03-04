73948ff0c70dab675fc35a420cf8f4a42b80466e
Amit Kandar <kandaramit2001@gmail.com>
2024-03-04 13:37:52 +0530

    Added some components

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Campaign from '../components/Campaign'
+import SellProduct from '../components/SellProduct'
+import SellBanner from '../components/SellBanner'
 
 function home() {
     return (
         <div className='bg-gray-100 px-5 py-5'>
             <Hero />
             <Campaign />
+            <SellProduct />
+            <SellBanner />
         </div>
     )
 }
